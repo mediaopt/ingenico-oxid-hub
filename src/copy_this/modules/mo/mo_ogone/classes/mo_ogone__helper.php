6ea3496b463c9bdbb1e596d63e79e9362acb4619
martin <martin.voerster@mediaopt.de>
2017-02-10 16:32:34 +0100

    #5278 added null check, to prevent warning message

--- src/copy_this/modules/mo/mo_ogone/classes/mo_ogone__helper.php
+++ src/copy_this/modules/mo/mo_ogone/classes/mo_ogone__helper.php
@@ -6,8 +6,14 @@ class mo_ogone__helper
     public function mo_ogone__isBrandActive($pm, $brand)
     {
         $options = oxRegistry::getConfig()->getConfigParam('mo_ogone__paymentOptions');
+
+        if (null === $options) {
+            return false;
+        }
+
         $option = $options[$pm];
-        return in_array($brand, $option);
+        return in_array($brand, $option, true);
+
     }
 
 }
