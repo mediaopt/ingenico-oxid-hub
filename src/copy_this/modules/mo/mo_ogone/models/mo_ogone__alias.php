620c1f013ed72b9fd74775d11a1b0ccd980dd63c
martin <martin.voerster@mediaopt.de>
2017-03-17 17:37:55 +0100

    #2194 first steps for alias saving

---
+++ src/copy_this/modules/mo/mo_ogone/models/mo_ogone__alias.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * For the full copyright and license information, refer to the accompanying LICENSE file.
+ *
+ * @copyright 2017 derksen mediaopt GmbH
+ */
+
+namespace models;
+
+/**
+ * model to store credit card alias data
+ *
+ * @author derksen mediaopt GmbH
+ * @package models
+ */
+class mo_ogone__alias
+{
+
+    /**
+     * Object core table name
+     *
+     * @var string
+     */
+    protected $_sCoreTable = 'mo_ogone__alias';
+
+    /**
+     * Current class name
+     *
+     * @var string
+     */
+    protected $_sClassName = 'mo_ogone__alias';
+}
